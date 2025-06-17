9a1502e2252dedfd91b96ef134dac395fe1c15d8
Decrescent398 <ganeshhgunda123@gmail.com>
2025-06-17 15:15:56 +0530

    some basic rx functions

--- solaris_app/solaris_app.py
+++ solaris_app/solaris_app.py
@@ -3,6 +3,10 @@ import reflex as rx
 class State(rx.State):
 
     count: int = 0
+    log: bool = False
+    head: str = ""
+    decider: bool = True
+    items: list[str] = ["Apple", "Banana", "Cherry"]
 
     @rx.event
     def increment(self):
@@ -11,6 +15,25 @@ class State(rx.State):
     @rx.event
     def decrement(self):
         self.count -= 1
+
+    @rx.event
+    def get_head(self, new_head):
+        self.head = new_head
+
+    @rx.event
+    def is_even(self):
+        if self.count % 2 == 0:
+            self.decider = True
+        else:
+            self.decider = False
+        
+    @rx.event
+    def toggle(self):
+        self.log = not self.log
+        State.is_even()
+
+def render_item(item: rx.Var[str]):
+    return rx.list.item(item)
 
 def index():
 
@@ -23,7 +46,17 @@ def index():
         rx.button("Decrement", color_scheme="ruby", on_click=State.decrement)
     ),
 
-    rx.progress(value=State.count)
+    rx.progress(value=State.count),
+    rx.input(default_value=State.head, on_blur=State.get_head),
+
+    rx.vstack(
+
+        rx.button(f"Toggle {State.head}", on_click=State.toggle),
+        rx.cond(State.decider, rx.heading(State.head), rx.heading(f"{State.head} not")),
+        rx.heading(State.head),
+        rx.foreach(State.items, render_item)
+
+    )
 
     )
 
