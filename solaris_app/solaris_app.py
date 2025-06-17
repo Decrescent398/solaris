d2567803e1c6b5dd7b0bcce1aa9174af93820c3e
Decrescent398 <ganeshhgunda123@gmail.com>
2025-06-17 14:51:10 +0530

    woah this is easy I built some stuff

--- solaris_app/solaris_app.py
+++ solaris_app/solaris_app.py
@@ -4,31 +4,27 @@ class State(rx.State):
 
     count: int = 0
 
+    @rx.event
     def increment(self):
         self.count += 1
 
+    @rx.event
     def decrement(self):
         self.count -= 1
 
 def index():
 
-    return rx.hstack(
+    return rx.box(
 
-        rx.button(
-            "Decrement",
-            color_scheme="ruby",
-            on_click=State.decrement,
-        ),
+    rx.hstack(
 
-        rx.heading(State.count, font_size="2em"),
+        rx.button("Increment", color_scheme="grass", on_click=State.increment),
+        rx.heading(State.count),
+        rx.button("Decrement", color_scheme="ruby", on_click=State.decrement)
+    ),
 
-        rx.button(
-            "Increment",
-            color_scheme="grass",
-            on_click=State.increment,
-        ),
+    rx.progress(value=State.count)
 
-        spacing = "4",
     )
 
 app = rx.App()
